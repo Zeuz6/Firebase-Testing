761a1764128514ccccb8b760a9a3c1a6eb4e9797
Zeuz Valdez <valdez.zeuz@gmail.com>
2025-12-17 17:12:28 -0600

    Added name input to index and register and added styles to signedin.html

--- scripts/signedIn.js
+++ scripts/signedIn.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
 // Same Firebase config you already use
 const firebaseConfig = {
@@ -16,13 +16,19 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 const message = document.getElementById("message");
+const logoutBtn = document.getElementById('logout');
 
 // Listen for logged-in user
 onAuthStateChanged(auth, (user) => {
   if (user) {
-    message.textContent = `Signed in ${user.email}`;
+    message.textContent = `Signed in as "${user.email}"`;
   } else {
     // If not logged in, redirect back to login
     window.location.href = "index.html";
   }
+});
+
+logoutBtn.addEventListener("click", async () => {
+  await signOut(auth);
+  window.location.href = "index.html";
 });
